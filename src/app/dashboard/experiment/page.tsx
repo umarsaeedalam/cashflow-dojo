9a358f4349f7daba63c4c7be4ee7bef3904a9934
“Umar <umarsaeedalam@gmail.com>
2024-10-28 13:43:13 +0500

    Made  some changes

--- src/app/dashboard/experiment/page.tsx
+++
@@ -1,9 +0,0 @@
-import ViewExpense from "@/components/feature/expenses/view-expense";
-
-export default function Page() {
-    return (
-        <div>
-            <ViewExpense />
-        </div>
-    )
-} 
