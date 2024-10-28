a20f5c4141b650dbe3b901007218d6fda59e149c
“Umar <umarsaeedalam@gmail.com>
2024-10-28 15:29:46 +0500

    Made  some changes more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,10 @@ const metadata: Metadata = {
 function RootLayout({ children, }: Readonly<{children: React.ReactNode;}>) {
     return (
         <html lang="en">
+            <head>
+                <meta name="color-scheme" content="light only" />
+            </head>
+            
             <body className={`${local.className} h-svh bg-primary text-secondary`}>
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ function LoginPage() {
                     <p className="text-3xl max-[700px]:text-2xl tracking-tight text-secondary font-normal mt-5 max-[1340px]:text-center max-[1340px]:mt-3 max-[550px]:hidden">Analyze your spending with comprehensive categorization and tracking for better financial control.</p>
                 </div>
 
-                <div className="max-w-md font-semibold text-primary border-secondary border-2 max-[550px]:bg-primary py-16 max-[1340px]:py-12 max-[700px]:py-8 px-8 max-[700px]:px-5 max-[550px]:px-4 rounded-xl flex flex-col items-center max-[550px]:border-primary max-[1340px]:self-center">
-                    <h1 className="text-3xl max-[700px]:text-2xl text-center max-[550px]:hidden text-secondary">Enter your financial dojo & begin tracking</h1>    
+                <div className="max-w-md font-semibold bg-secondary text-primary border-secondary border-2 max-[550px]:bg-primary py-16 max-[1340px]:py-12 max-[700px]:py-8 px-8 max-[700px]:px-5 max-[550px]:px-4 rounded-xl flex flex-col items-center max-[550px]:border-primary max-[1340px]:self-center">
+                    <h1 className="text-3xl max-[700px]:text-2xl text-center max-[550px]:hidden text-neutral">Enter your financial dojo & begin tracking</h1>    
 
                     <form action={signInGoogle}>
                         <button type="submit" className="flex items-center gap-2 mt-10 max-[550px]:mt-1 max-[700px]:mt-6 bg-accent text-primary rounded-lg py-2 px-20 max-[700px]:px-16 max-[440px]:px-11 border border-accent hover:bg-accent-shade outline-none focus-visible:outline-secondary transition-all ease-in-out duration-200 transform active:scale-90">
@@ -33,7 +33,7 @@ function LoginPage() {
                         </button>
                     </form>
 
-                    <p className="mt-2 text-secondary max-[700px]:text-sm max-[550px]:text-primary">or</p>
+                    <p className="mt-2 text-neutral max-[700px]:text-sm max-[550px]:text-primary">or</p>
 
                     <form action={signInGithub}>
                         <button type="submit" className="flex items-center gap-2 mt-2 bg-accent text-primary rounded-lg py-2 px-20 max-[700px]:px-16 max-[440px]:px-11 border border-accent hover:bg-accent-shade hover:border-accent-shade outline-none focus-visible:outline-secondary transition-all ease-in-out duration-200 transform active:scale-90">
